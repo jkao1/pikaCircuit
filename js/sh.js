b6800a5c68ef36268857fa4c6e06dab9be985da6
jkao1 <jkao1@stuy.edu>
2016-05-28 21:31:38 -0400

    updated for the dassy

--- js/sh.js
+++ js/sh.js
@@ -20,20 +20,20 @@ batt = battery
 */
 
 function start() { // initiates game
-    pl_wire01 = new component(150, 10, "#ABB482", 150, 300);
-    pl_wire02 = new component(10, 180, "#ABB482", 240, 369); 
-    pl_wire03 = new component(10, 480, "#ABB482", 600, 355); 
-    pl_wire04 = new component(150, 10, "#ABB482", 800, 282); 
-    pl_retr05 = new component(10, 80, "#BEC991", 350, 365);
+    pl_wire01 = new component(150, 10, "#abb482", 150, 300);
+    pl_wire02 = new component(10, 180, "#abb482", 240, 369); 
+    pl_wire03 = new component(10, 440, "#abb482", 580, 369); 
+    pl_wire04 = new component(184, 10, "#abb482", 800, 282); 
+    pl_retr05 = new component(10, 80, "#bec991", 350, 369);
     
-    wire01 = new component(150, 10, "#D7912F", 70, 300); 
-    wire02 = new component(10, 180, "#D7912F", 480, 495); 
-    wire03 = new component(10, 480, "#D7912F", 500, 525); 
-    wire04 = new component(150, 10, "#D7912F", 900, 200); 
-    retr05 = new component(25, 115.5, "retrimg", 450, 455, "img");
+    wire01 = new component(150, 10, "#d7912f", 70, 300); 
+    wire02 = new component(10, 180, "#d7912f", 480, 495); 
+    wire03 = new component(10, 440, "#d7912f", 500, 525); 
+    wire04 = new component(184, 10, "#d7912f", 900, 200); 
+    retr05 = new component(25, 115.5, "retr05", 450, 455, "img");
     
-    lemd06 = new component(80, 80, "lightimg", 800, 180, "img");
-    batt07 = new component(115, 80, "battimg", 150, 180, "img");
+    lemd06 = new component(80, 80, "lemd06", 800, 180, "img");
+    batt07 = new component(115, 80, "batt07", 150, 180, "img");
     
     char = new component(15, 15, "#fa8940", 250, 265);
 
@@ -146,7 +146,9 @@ function component(width, height, color, x, y, type) {
         b = myleft > otherright;
         c = myright < otherleft;
         d = mybottom < othertop;
+        
         var crash = true;
+        
         if (a||b||c||d) {
             crash = false;
         } 
